e1d7901a8020aea8c4bb8206d654518c9aa863d7
Guillaume Villena <guillaume@villena.me>
2022-03-18 00:52:19 +0100

    Updated gradle scripts and implemented whitelist feature as requested in #9

--- app/src/main/java/com/github/willena/android_phonenumber_input/MainActivity.java
+++ app/src/main/java/com/github/willena/android_phonenumber_input/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
     private PhoneInputView phoneView1;
     private Button b;
-    private PhoneInputView phoneView2;
-    private PhoneInputView phoneView0;
-    private PhoneInputView phoneView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,39 +21,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Default config
-        phoneView0 = (PhoneInputView) findViewById(R.id.phoneId1);
+        PhoneInputView phoneView0 = findViewById(R.id.phoneId1);
         phoneView0.setConfig(new CountryConfigurator());
 
 
         // Define number and country
-        b = (Button) findViewById(R.id.button_validate2);
-        phoneView1 = (PhoneInputView) findViewById(R.id.phoneId2);
+        b = findViewById(R.id.button_validate2);
+        phoneView1 = findViewById(R.id.phoneId2);
         phoneView1.setConfig(new CountryConfigurator());
-        phoneView1.addOnValidEntryListener(new OnValidEntryListener() {
-            @Override
-            public void onValidEntry(Boolean valid) {
-                b.setEnabled(valid);
-                Log.d("MAINACTIVITY", "onValidEntry: " + phoneView1.getFormatedNumber());
-            }
+        phoneView1.addOnValidEntryListener(valid -> {
+            b.setEnabled(valid);
+            Log.d("MAINACTIVITY", "onValidEntry: " + phoneView1.getFormatedNumber());
         });
         phoneView1.setPhoneNumber("0102030405", "FR");
 
 
         //Define default country value
-        phoneView2 = (PhoneInputView) findViewById(R.id.phoneId3);
+        PhoneInputView phoneView2 = findViewById(R.id.phoneId3);
         CountryConfigurator config2 = new CountryConfigurator();
         config2.setDefaultCountry("US");
         phoneView2.setConfig(config2);
 
 
         // Only flag and code
-        phoneView4 = (PhoneInputView) findViewById(R.id.phoneId4);
+        PhoneInputView phoneView4 = findViewById(R.id.phoneId4);
         CountryConfigurator config = new CountryConfigurator();
         config.setDisplayFlag(true);
         config.setDisplayCountryCode(true);
         config.setDisplayDialingCode(false);
         config.setPhoneNumberHintType(null);
-
         phoneView4.setConfig(config);
+
+
+        // Using whitelisted countries
+        PhoneInputView phoneView5 = findViewById(R.id.phoneId5);
+        CountryConfigurator config3 = new CountryConfigurator();
+        config3.setDefaultCountry("GB");
+        config3.setCountryWhitelist("FR", "GB", "US");
+        phoneView5.setConfig(config3);
+
     }
 }
